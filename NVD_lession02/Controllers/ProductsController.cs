390fb00fb37552d071bea1d6a8758f5acb48a2f9
DuocTerbook <nguyenvanduoc09665362766276@gmail.com>
2024-05-08 18:03:19 +0700

    first commit

---
+++ NVD_lession02/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NVD_lession02.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products ~ http://localhost:port/products
+        public ActionResult Index()
+        {
+            ViewBag.name = "Duoc Nguyen - 2210900016";
+            return View();
+        }
+        public ActionResult Details(int id)
+        {
+            ViewBag.id = id;
+            return View();
+        }
+        public string GetName()
+        {
+            return "Nguyễn Văn Được - 2210900016";
+        }
+        public JsonResult ListName()
+        {
+            string[] name = { "Linh", "Lan", "Hùng", "Dũng" };
+            return Json(name, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ NVD_lession02/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NVD_lession02.MvcApplication" Language="C#" %>
